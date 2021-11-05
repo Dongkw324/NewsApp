f5dcd4ae900dc1646776ed959fbabe0c1f2e40b8
Dongkw324 <59963677+Dongkw324@users.noreply.github.com>
2021-11-05 18:20:44 +0900

    news_app Api Module 구현

---
+++ app/src/main/java/com/kdw/newsapp/di/ApiModule.kt
@@ -0,0 +1,56 @@
+package com.kdw.newsapp.di
+
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.kdw.newsapp.BuildConfig
+import com.kdw.newsapp.api.NewsApi
+import com.kdw.newsapp.util.Constant
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.Protocol
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.*
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class ApiModule {
+
+    @Singleton
+    @Provides
+    fun provideGson(): Gson = GsonBuilder().setLenient().create()
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(gson: Gson): Retrofit = Retrofit.Builder()
+        .baseUrl(Constant.baseUrl)
+        .client(OkHttpClient.Builder().also { client ->
+            if(BuildConfig.DEBUG) {
+                val logging = HttpLoggingInterceptor()
+                logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+                client.addInterceptor(logging)
+                client.connectTimeout(120, TimeUnit.SECONDS)
+                client.readTimeout(120, TimeUnit.SECONDS)
+                client.protocols(Collections.singletonList(Protocol.HTTP_1_1))
+            }
+        }.build()
+        )
+        .addConverterFactory(GsonConverterFactory.create(gson))
+        .build()
+
+
+    @Singleton
+    @Provides
+    fun provideApi(retrofit: Retrofit.Builder) : NewsApi {
+        return retrofit
+            .build()
+            .create(NewsApi::class.java)
+
+    }
+}
