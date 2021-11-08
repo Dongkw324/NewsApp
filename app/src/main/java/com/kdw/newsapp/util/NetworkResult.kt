885ad1cdd2fcb637ca1dfdf475b47e5496a40331
Dongkw324 <59963677+Dongkw324@users.noreply.github.com>
2021-11-08 11:00:07 +0900

    news_app Retrofit 응답 상태 관리

---
+++ app/src/main/java/com/kdw/newsapp/util/NetworkResult.kt
@@ -0,0 +1,8 @@
+package com.kdw.newsapp.util
+
+import java.lang.Exception
+
+sealed class NetworkResult<out T: Any> {
+    data class Success<out T: Any>(val data: T) : NetworkResult<T>()
+    data class Error(val exception: Exception) : NetworkResult<Nothing>()
+}
