21a9637e1c1b274f1d7b705ccdf857837a41632b
Dongkw324 <59963677+Dongkw324@users.noreply.github.com>
2021-11-22 11:25:11 +0900

    news_app Article Model 및 Hilt 관련 오류 수정

--- app/src/main/java/com/kdw/newsapp/model/Article.kt
+++ app/src/main/java/com/kdw/newsapp/model/Article.kt
@@ -2,17 +2,20 @@ package com.kdw.newsapp.model
 
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 import java.io.Serializable
 
 @Entity(tableName = "articleTable")
 data class Article(
-    val author: String,
-    val content: Any,
-    val description: String,
-    val publishedAt: String,
-    val source: Source,
-    val title: String,
-    val url: String,
-    val urlToImage: String
+    @PrimaryKey(autoGenerate = true)
+    var id: Int? = null,
+    val author: String?,
+    val content: String?,
+    val description: String?,
+    val publishedAt: String?,
+    val source: Source?,
+    val title: String?,
+    val url: String?,
+    val urlToImage: String?
 ) : Serializable
--- app/src/main/java/com/kdw/newsapp/model/Source.kt
+++ app/src/main/java/com/kdw/newsapp/model/Source.kt
@@ -4,6 +4,6 @@ package com.kdw.newsapp.model
 import java.io.Serializable
 
 data class Source(
-    val id: Any,
-    val name: String
+    val id: Any?,
+    val name: String?
 ) : Serializable
