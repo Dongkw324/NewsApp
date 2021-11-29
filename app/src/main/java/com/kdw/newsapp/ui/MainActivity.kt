230f231a0c11c346fc920f5b28961acb6343dfd4
Dongkw324 <59963677+Dongkw324@users.noreply.github.com>
2021-11-29 12:29:25 +0900

    news_app MainActivity 수정

--- app/src/main/java/com/kdw/newsapp/ui/MainActivity.kt
+++ app/src/main/java/com/kdw/newsapp/ui/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.kdw.newsapp.R
 import com.kdw.newsapp.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -17,15 +18,14 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        _binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        val bottomMenu = findViewById<BottomNavigationView>(R.id.bottom_menu)
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
         val navController = navHostFragment.navController
 
-        binding.bottomMenu.setupWithNavController(navController)
-
+        bottomMenu.setupWithNavController(navController)
     }
 
 }
