5a514d235994ace9b346036df6ee05263b4c3afb
Dongkw324 <59963677+Dongkw324@users.noreply.github.com>
2021-12-28 14:07:30 +0900

    news_app detailArticleFragment 구현

--- app/src/main/java/com/kdw/newsapp/ui/fragment/DetailArticleFragment.kt
+++ app/src/main/java/com/kdw/newsapp/ui/fragment/DetailArticleFragment.kt
@@ -4,9 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
 import com.kdw.newsapp.R
 import com.kdw.newsapp.databinding.FragmentDetailArticleBinding
+import com.kdw.newsapp.viewModel.ArticleViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -15,6 +19,9 @@ class DetailArticleFragment: Fragment(R.layout.fragment_detail_article) {
     private var _detailBinding : FragmentDetailArticleBinding? = null
     private val detailBinding get() = _detailBinding!!
 
+    private val detailViewModel : ArticleViewModel by viewModels()
+    private val args : DetailArticleFragmentArgs by navArgs()
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -22,6 +29,19 @@ class DetailArticleFragment: Fragment(R.layout.fragment_detail_article) {
     ): View {
         _detailBinding = FragmentDetailArticleBinding.inflate(inflater, container, false)
         return detailBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val articleView = args.article
+
+        detailBinding.articleWebView.apply {
+            webViewClient = WebViewClient()
+            articleView.url?.let {
+                loadUrl(it)
+            }
+        }
+
     }
 
     override fun onDestroyView() {
