0dea3bcdbaa9a07da96b19e5f4e34b2a7f3f8eb8
Dongkw324 <59963677+Dongkw324@users.noreply.github.com>
2022-01-06 11:41:35 +0900

    news_app SavedArticleFragment 구현

--- app/src/main/java/com/kdw/newsapp/ui/fragment/SavedArticleFragment.kt
+++ app/src/main/java/com/kdw/newsapp/ui/fragment/SavedArticleFragment.kt
@@ -1,5 +1,6 @@
 package com.kdw.newsapp.ui.fragment
 
+import android.media.browse.MediaBrowser
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,7 +9,11 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 import com.kdw.newsapp.R
 import com.kdw.newsapp.adapter.ArticleAdapter
 import com.kdw.newsapp.databinding.FragmentDetailArticleBinding
@@ -36,6 +41,30 @@ class SavedArticleFragment: Fragment(R.layout.fragment_saved_article) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val touchHelper = object : ItemTouchHelper.SimpleCallback(
+            ItemTouchHelper.UP or ItemTouchHelper.DOWN,
+            ItemTouchHelper.RIGHT or ItemTouchHelper.LEFT
+        ) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return true
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+                val article = savedArticleAdapter.differ.currentList[position]
+                articleViewModel.deletedNews(article)
+
+                Snackbar.make(savingBinding.root, "뉴스 삭제 완료", Snackbar.LENGTH_LONG)
+                    .show()
+            }
+        }
+
+        ItemTouchHelper(touchHelper).attachToRecyclerView(savingBinding.savedArticleRecyclerView)
 
         savedArticleAdapter = ArticleAdapter()
         savingBinding.savedArticleRecyclerView.apply {
