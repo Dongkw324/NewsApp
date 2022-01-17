d6a419399f4647626b297b8f5b755feb3d10f6d1
Dongkw324 <59963677+Dongkw324@users.noreply.github.com>
2022-01-17 13:15:51 +0900

    news_app SavedArticleFragment 구현

--- app/src/main/java/com/kdw/newsapp/ui/MainActivity.kt
+++ app/src/main/java/com/kdw/newsapp/ui/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.kdw.newsapp.ui
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.RequiresApi
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.kdw.newsapp.R
 import com.kdw.newsapp.databinding.ActivityMainBinding
+import com.kdw.newsapp.util.StatusTransport
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -16,9 +19,12 @@ class MainActivity : AppCompatActivity() {
    // private var _binding: ActivityMainBinding? = null
    // private val binding get() = _binding!!
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        StatusTransport.makeStatusTransport(this@MainActivity)
 
         val bottomMenu = findViewById<BottomNavigationView>(R.id.bottom_menu)
 
